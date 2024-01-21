c3405b055e889a53482ae2e79212a639d68ca408
Pandey Bikash <bkppandey87@gmail.com>
2024-01-21 21:45:26 +0545

    Do my_each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,24 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    i = 0
+    new_array = []
+    while i < self.length
+      new_array << yield(self[i])
+      i += 1
+    end
+    new_array
+  end
+
+  def my_each
+    new_array = []
+    for individual_element in self
+      new_array << yield(individual_element)
+    end
+  end
 end
+
+
+# a = [1, 2, 3]
+# p a.my_each { |n| n }
